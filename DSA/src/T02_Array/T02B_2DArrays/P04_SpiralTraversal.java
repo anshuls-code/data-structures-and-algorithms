1641e4a5abab54e5ae67d9a2e5b1c98e13db9f82
Anshul Sharma <anshulsharma.code@gmail.com>
2026-09-23 12:27:01 +0530

    initial commit

--- DSA/src/T02_Array/T02B_2DArrays/P04_SpiralTraversal.java
+++
@@ -1,8 +0,0 @@
-package T02_Array.T02B_2DArrays;
-
-public class P04_SpiralTraversal {
-
-    static void main() {
-        int[][] arr = {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}};
-    }
-}
